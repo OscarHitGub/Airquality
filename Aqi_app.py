1236c4565e0dfa1f4a933e76bd8d0b00b655c8af
OscarHitGub <oscar.bastiaansen@hva.nl>
2026-01-22 13:29:39 +0100

    Update Aqi_app.py

--- Aqi_app.py
+++ Aqi_app.py
@@ -316,8 +316,8 @@ if App_selection == "Training data":
         text_alignment="center",
         width="stretch"
     )
-    st.text("Bron voor temperatuur: https://www.mt-dashboard.nl/admin/d/xbR4dVq4z\nBron voor regen en wind: https://www.knmi.nl/nederland-nu/klimatologie/daggegevens")
-    st.page_link("https://www.mt-dashboard.nl/admin/d/xbR4dVq4z", label="Bron voor temperatuur")
+    st.text("Bron voor temperatuur en luchtkwaliteit: https://www.mt-dashboard.nl/admin/d/xbR4dVq4z\nBron voor regen en wind: https://www.knmi.nl/nederland-nu/klimatologie/daggegevens")
+
 if App_selection == "Model omschrijving":
     st.markdown("""
 ## Modelbeschrijving – Gradient Boosting Regressor
@@ -518,6 +518,7 @@ Ook hier kan een voorspelling van *matige luchtkwaliteit* in werkelijkheid *goed
 Bron:
 https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsRegressor.html
 """)
+
 
 
 
